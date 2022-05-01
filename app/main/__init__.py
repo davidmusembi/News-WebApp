f5a6a6de9509b175f6f1d1a35ef02a525541ae7b
davidmusembi <davidmusembi02@gmail.com>
2022-05-02 01:49:59 +0300

    Add configuratons

---
+++ app/main/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+main = Blueprint('main', __name__)
+from . import views,error
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
