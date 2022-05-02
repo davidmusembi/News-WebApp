03b89d46b6bf75acbafc8a1d223cca098b88816d
davidmusembi <davidmusembi02@gmail.com>
2022-05-02 10:41:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY=3b47014ee72a44118cdc3520fe0d0d84
-export SECRET_KEY=PIEDPIPER4186
+export NEWS_API_KEY='3b47014ee72a44118cdc3520fe0d0d84'
+export SECRET_KEY='PIEDPIPER4186'
 
 python3 manage.py server
